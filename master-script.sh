4fabb80aaeb92aeaaa3a10e60b976db422c0516c
Eyal Ben Ivri <eyalbenivri@gmail.com>
2016-06-23 08:05:48 +0300

    restart zepp

--- master-script.sh
+++ master-script.sh
@@ -1,3 +1,3 @@
 sudo ~/Downloads/spark-161/sbin/start-master.sh
 sudo ~/Downloads/spark-161/sbin/start-slave.sh -c 2 -m 2G spark://Spark-VM:7077
-sudo ~/Downloads/zeppelin-0.5.6-incubating/bin/zeppelin-daemon.sh start
+sudo ~/Downloads/zeppelin-0.5.6-incubating/bin/zeppelin-daemon.sh restart
